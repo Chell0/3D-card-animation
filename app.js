124ae8ad15a4f901bbef5d31e7bc4b4cbac86418
Chell0 <machelgabriel@gmail.com>
2020-11-26 03:26:08 +0300

    Add animation

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const container = document.querySelector(".container");
 // Moving Animation Event
 container.addEventListener("mousemove", (e) => {
     let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-    let yAxis = (window.innerWidth / 2 - e.pageY) / 10;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
